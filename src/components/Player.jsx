5e1895af3f267fe62e4698d6acfc9a65525c48ff
Neha Reddy <nehareddy@Nehas-MBP.lan>
2026-03-22 23:01:18 -0500

    accessing HTML elements via Refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
+
   const [name, setName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
 
   function handleClick() {
-    setSubmitted(true);
+    setName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? name : "unknown entity"}</h2>
+      <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
         <input
+          ref={playerName}
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
         />
         <button onClick={handleClick} >Set Name</button>
       </p>
